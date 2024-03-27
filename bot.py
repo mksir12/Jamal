1376e916821ac547ce6c471c0a8f5f96ee6be4c7
mksir12 <116490353+mksir12@users.noreply.github.com>
2024-03-27 15:23:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import re
 import random
 
 import requests
-from lxml import html
 from telegram.ext import Updater, CommandHandler
 
 DEBUG = 1
@@ -46,34 +45,6 @@ def video_at(bot, update):
         return
 
     update.message.reply_text("https://youtube.com" + random.choice(video_list)[6:-1])
-
-def haber_at(bot, update):
-    __debug("CALL haber_at")
-
-    __debug("haber_at: Sending request to https://www.bbc.com/news")
-
-    pg = requests.get("https://www.bbc.com/news")
-
-    __debug("haber_at: Got response: %d" % pg.status_code)
-
-    if pg.status_code != 200:
-        update.message.reply_text("Aman! Bir hata oldu :(")
-        return
-
-    tree = html.fromstring(pg.text)
-
-    news_list = list()
-    for i in range(1, 11):
-        elem = tree.xpath("//li[@data-entityid='most-popular-read-{}']//a".format(i))
-
-        if elem != []:
-            news_list.append(elem[0])
-
-    if len(news_list) == 0:
-        update.message.reply_text("Aman! Bir hata oldu! Hiç haber bulamadım :(")
-        return
-
-    update.message.reply_text("https://www.bbc.com" + random.choice(news_list).attrib["href"])
 
 def main():
     __debug("CALL main")
